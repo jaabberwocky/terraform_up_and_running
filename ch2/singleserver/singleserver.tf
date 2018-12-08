be8281b13c589d315388b6335b7a4d483adc7fb9
tobias <tobias@data.gov.sg>
2018-12-08 11:52:33 +0800

    finished configurable web server

--- ch2/singleserver/singleserver.tf
+++ ch2/singleserver/singleserver.tf
@@ -1,7 +1,42 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "server_port" {
+  type = "string"
+  description = "The port the server will use for HTTP requests"
+  default = "8080"
+}
+
 resource "aws_instance" "example" {
-	ami = "ami-004dc87ccb133be6b"
-	instance_type = "t2.micro"
-	tags {
-		Name = "terraform-example"
-	}
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  associate_public_ip_address = "true"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p "${var.server_port}" &
+    EOF
+
+  tags {
+    Name="terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "allow-terraform-instance-http"
+  
+
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
